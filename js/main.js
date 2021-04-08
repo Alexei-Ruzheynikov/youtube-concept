c17a652a2e07641936d85635fa33205911e98956
Alexei-Ruzheynikov <80196598+Alexei-Ruzheynikov@users.noreply.github.com>
2021-04-08 22:11:31 +0300

    Верстка центрального блока, фиксированная шапка и боковое меню

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper(".channel-slider", {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-button-next",
+    prevEl: ".channel-button-prev",
+  },
+});
+
+const swiper2 = new Swiper(".channel-slider2", {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-button-next2",
+    prevEl: ".channel-button-prev2",
+  },
+});
+const swiper3 = new Swiper(".channel-slider3", {
+  // Optional parameters
+
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".channel-button-next3",
+    prevEl: ".channel-button-prev3",
+  },
+});
